e51e9e5aaa5e718b76fb8146a2126d838e51ab14
Vuuubi <gamingnight998@gmail.com>
2022-01-02 02:28:21 +0100

    Basic Controllers Setup

---
+++ Friendstagram-Backend/Controllers/FriendstagramControllerBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+using Friendstagram_Backend.Model;
+
+namespace Friendstagram_Backend.Controllers
+{
+    [ApiController]
+    [Route("api/{GroupCode}")]
+    public class FriendstagramControllerBase : ControllerBase
+    {
+        [HttpGet]
+        public Group Get(string GroupCode)
+        {
+            using (FriendstagramContext DBContext = new FriendstagramContext())
+            {
+                if (DBContext.Database.CanConnect())
+                {
+                    return DBContext.Groups.FirstOrDefault(g => g.Code == GroupCode);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
